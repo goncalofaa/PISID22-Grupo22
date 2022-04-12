d2dce631bad101b3bd7da672804113334bf4de2d
tiagosil <tabss1@iscte-iul.pt>
2022-04-12 01:14:22 +0100

    Changes MongoToMongo Last Changes PISID

--- src/main/java/MongoToMQTTTest.java
+++ src/main/java/MongoToMQTTTest.java
@@ -14,24 +14,22 @@ import java.util.UUID;
 
 public class MongoToMQTTTest {
 
-    // Local dadtabase
     private static String databaselocal = "medicoes";
 
-    //CloudServer
-    private static String clouServer = "tcp://broker.mqtt-dashboard.com:1883";
+    // PERGUNTAR OU VER SE PODEMOS USAR ESTE QUE Ã‰ O DO PROF FÃ¡bio
+    private String cloudServer = "tcp://broker.mqtt-dashboard.com:1883";
 
-    // Url to access to local replics
-    String urlLocal = "mongodb://localhost:27019,localhost:25019,localhost:23019/?replSet=replicaimdb";
+    // Url para aceder ás réplicas locais
+    String urlLocal = "mongodb://localhost:27019,localhost:25019,localhost:23019/?replicaSet=medicoespisid";
 
-    //Topics to subscribe in Broker
     private String topicot1 = "G22T1";
-    private String topicot2 = "G22T2";
+    private String topicot2 = "G22T1";
     private String topicoh1 = "G22H1";
     private String topicoh2 = "G22H2";
     private String topicol1 = "G22L1";
     private String topicol2 = "G22L2";
 
-    //Strings to local Collections
+    // locais
     private static String collectiont1 = "t1";
     private static String collectiont2 = "t2";
     private static String collectionh1 = "h1";
@@ -39,24 +37,7 @@ public class MongoToMQTTTest {
     private static String collectionl1 = "l1";
     private static String collectionl2 = "l2";
 
-    //Striengs to the last Date
-    private static String lastLocalDatet1;
-    private static String lastLocalDatet2;
-    private static String lastLocalDateh1;
-    private static String lastLocalDateh2;
-    private static String lastLocalDatel1;
-    private static String lastLocalDatel2;
-
-
-    //Validate if collections are empty or not
-    private static Boolean t1vazia = false;
-    private static Boolean t2vazia = false;
-    private static Boolean h1vazia = false;
-    private static Boolean h2vazia = false;
-    private static Boolean l1vazia = false;
-    private static Boolean l2vazia = false;
-
-    //Local Collections
+    // coleÃ§Ãµes locais
     private static MongoCollection<Document> localcollectiont1;
     private static MongoCollection<Document> localcollectiont2;
     private static MongoCollection<Document> localcollectionh1;
@@ -64,34 +45,22 @@ public class MongoToMQTTTest {
     private static MongoCollection<Document> localcollectionl1;
     private static MongoCollection<Document> localcollectionl2;
 
-    //Local String of the last Document
-    private String stringDocumentt1;
-    private String stringDocumentt2;
-    private String stringDocumentl1;
-    private String stringDocumentl2;
-    private String stringDocumenth1;
-    private String stringDocumenth2;
-
-    //String of cloud dates
-    private static String cloudLastDatet1;
-    private static String cloudLastDatet2;
-    private static String cloudLastDatel1;
-    private static String cloudLastDatel2;
-    private static String cloudLastDateh1;
-    private static String cloudLastDateh2;
-
-    //Local Client
-    private static MongoClient localMongoClient;
+    //Últimas datas de cada coleção
+    private static String datat1;
+    private static String datat2;
+    private static String datah1;
+    private static String datah2;
+    private static String datal1;
+    private static String datal2;
 
     MongoClient localClient = new MongoClient(new MongoClientURI(urlLocal));
     MongoDatabase localMongoDatabase = localClient.getDatabase(databaselocal);
-
-    private IMqttClient mqttClient;
+    IMqttClient mqttClient;
 
     public void connectToMQTT() throws MqttException {
 
         String clientId = UUID.randomUUID().toString();
-        mqttClient = new MqttClient(clouServer, clientId);
+        mqttClient = new MqttClient(cloudServer, clientId);
 
         MqttConnectOptions options = new MqttConnectOptions();
         options.setAutomaticReconnect(true);
@@ -100,175 +69,161 @@ public class MongoToMQTTTest {
         mqttClient.connect(options);
     }
 
-    public void migrate() throws MqttPersistenceException, MqttException, InterruptedException {
+    public void migrate(String lastDate) throws MqttPersistenceException, MqttException, InterruptedException {
 
         // Tratamento da coleção t1 local
         localcollectiont1 = localMongoDatabase.getCollection(collectiont1);
-
-        Document dt1 = localcollectiont1.find().sort(new BasicDBObject("_id", -1)).first();
-
-        if (collectiont1.equals("t1")) {
-            Thread.sleep(5000);
-
-            String zonat1 = dt1.getString("Zona");
-            String sensort1 = dt1.getString("Sensor");
-            String datat1 = dt1.getString("Data").replace("T", " ").replace("Z", " ").replace("-", "").replace(":", "");
-            String medicaot1 = dt1.getString("Medicao");
-            stringDocumentt1 = zonat1 + "|" + sensort1 + "|" + datat1 + "|" + medicaot1;
-            byte[] arrayBytet1 = stringDocumentt1.getBytes();
-
-
-            MqttMessage mqttMessaget1 = new MqttMessage(arrayBytet1);
-            mqttMessaget1.setQos(0);
-            mqttMessaget1.setRetained(false);
-            mqttClient.publish(topicot1, mqttMessaget1);
-            System.out.println(mqttMessaget1.toString());
-
+        FindIterable<Document> newLocalMeasurementst1;
+        if (lastDate == null) {
+            newLocalMeasurementst1 = localcollectiont1.find();
         } else {
-            return;
+            newLocalMeasurementst1 = localcollectiont1.find(Filters.gt("Data", lastDate));
         }
-
-
+        for (Document dt1 : newLocalMeasurementst1) {
+            if (collectiont1.equals("t1")) {
+                String zonat1 = dt1.getString("Zona");
+                String sensort1 = dt1.getString("Sensor");
+                datat1 = dt1.getString("Data").replace("T", " ").replace("Z", " ").replace("-", "").replace(":", "");
+                String medicaot1 = dt1.getString("Medicao");
+                String stringDocumentt1 = zonat1 + "|" + sensort1 + "|" + datat1 + "|" + medicaot1;
+                byte[] arrayBytet1 = stringDocumentt1.getBytes();
+                MqttMessage mqttMessaget1 = new MqttMessage(arrayBytet1);
+                mqttMessaget1.setQos(0);
+                mqttMessaget1.setRetained(false);
+                mqttClient.publish(topicot1, mqttMessaget1);
+                System.out.println(mqttMessaget1.toString());
+            } else {
+                return;
+            }
+        }
         // Tratamento da coleção t2 local
         localcollectiont2 = localMongoDatabase.getCollection(collectiont2);
-
-        Document dt2 = localcollectiont2.find().sort(new BasicDBObject("_id", -1)).first();
-
+        FindIterable<Document> newLocalMeasurementst2;
+        if (lastDate == null) {
+            newLocalMeasurementst2 = localcollectiont2.find();
+        } else {
+            newLocalMeasurementst2 = localcollectiont2.find(Filters.gt("Data", lastDate));
+        }
+        for (Document dt2 : newLocalMeasurementst1) {
             if (collectiont2.equals("t2")) {
-                Thread.sleep(5000);
+                String zonat2 = dt2.getString("Zona");
+                String sensort2 = dt2.getString("Sensor");
+                datat2 = dt2.getString("Data").replace("T", " ").replace("Z", " ").replace("-", "").replace(":", "");
+                String medicaot2 = dt2.getString("Medicao");
+                String stringDocumentt2 = zonat2 + "|" + sensort2 + "|" + datat2 + "|" + medicaot2;
 
-
-                    String zonat2 = dt2.getString("Zona");
-                    String sensort2 = dt2.getString("Sensor");
-                    String datat2 = dt2.getString("Data").replace("T", " ").replace("Z"," ").replace("-","").replace(":", "");
-                    String medicaot2 = dt2.getString("Medicao");
-                    stringDocumentt2 = zonat2 + "|" + sensort2 + "|" + datat2 + "|" + medicaot2;
-
-                    byte[] arrayBytet2 = stringDocumentt2.getBytes();
-
-                    MqttMessage mqttMessaget2 = new MqttMessage(arrayBytet2);
-                    mqttMessaget2.setQos(0);
-                    mqttMessaget2.setRetained(false);
-                    mqttClient.publish(topicot2, mqttMessaget2);
-                    System.out.println(mqttMessaget2.toString());
-
-
+                byte[] arrayBytet2 = stringDocumentt2.getBytes();
+                MqttMessage mqttMessaget2 = new MqttMessage(arrayBytet2);
+                mqttMessaget2.setQos(0);
+                mqttMessaget2.setRetained(false);
+                mqttClient.publish(topicot2, mqttMessaget2);
+                System.out.println(mqttMessaget2.toString());
             } else {
                 return;
             }
-
-
-
+        }
         // Tratamento da coleção h1 local
         localcollectionh1 = localMongoDatabase.getCollection(collectionh1);
-
-        Document dh1 = localcollectionh1.find().sort(new BasicDBObject("_id", -1)).first();
-
+        FindIterable<Document> newLocalMeasurementsh1;
+        if (lastDate == null) {
+            newLocalMeasurementsh1 = localcollectionh1.find();
+        } else {
+            newLocalMeasurementsh1 = localcollectionh1.find(Filters.gt("Data", lastDate));
+        }
+        for (Document dh1 : newLocalMeasurementsh1) {
             if (collectionh1.equals("h1")) {
-                Thread.sleep(5000);
-
                 String zonah1 = dh1.getString("Zona");
                 String sensorh1 = dh1.getString("Sensor");
-                String datah1 = dh1.getString("Data").replace("T", " ").replace("Z"," ").replace("-","").replace(":", "");
+                datah1 = dh1.getString("Data").replace("T", " ").replace("Z", " ").replace("-", "").replace(":", "");
                 String medicaoh1 = dh1.getString("Medicao");
-                stringDocumenth1 = zonah1 + "|" + sensorh1 + "|" + datah1 + "|" + medicaoh1;
-
-                    byte[] arrayByteh1 = stringDocumenth1.getBytes();
-
-                    MqttMessage mqttMessageh1 = new MqttMessage(arrayByteh1);
-                    mqttMessageh1.setQos(0);
-                    mqttMessageh1.setRetained(false);
-                    mqttClient.publish(topicoh1, mqttMessageh1);
-                    System.out.println(mqttMessageh1.toString());
-
+                String stringDocumenth1 = zonah1 + "|" + sensorh1 + "|" + datah1 + "|" + medicaoh1;
+                byte[] arrayByteh1 = stringDocumenth1.getBytes();
+                MqttMessage mqttMessageh1 = new MqttMessage(arrayByteh1);
+                mqttMessageh1.setQos(0);
+                mqttMessageh1.setRetained(false);
+                mqttClient.publish(topicoh1, mqttMessageh1);
+                System.out.println(mqttMessageh1.toString());
             } else {
                 return;
             }
-
-
-
+        }
         // Tratamento da coleção h2 local
         localcollectionh2 = localMongoDatabase.getCollection(collectionh2);
-
-        Document dh2 = localcollectionh2.find().sort(new BasicDBObject("_id", -1)).first();
-
+        FindIterable<Document> newLocalMeasurementsh2;
+        if (lastDate == null) {
+            newLocalMeasurementsh2 = localcollectionh2.find();
+        } else {
+            newLocalMeasurementsh2 = localcollectionh2.find(Filters.gt("Data", lastDate));
+        }
+        for (Document dh2 : newLocalMeasurementsh2) {
             if (collectionh2.equals("h2")) {
-                Thread.sleep(5000);
-
-                    String zonah2 = dh2.getString("Zona");
-                    String sensorh2 =  dh2.getString("Sensor");
-                    String datah2 = dh2.getString("Data").replace("T", " ").replace("Z", " ");
-                    String medicaoh2 = dh2.getString("Medicao");
-                    stringDocumenth2 = zonah2 + "|" + sensorh2 + "|" + datah2 + "|" + medicaoh2;
-
-                    byte[] arrayByteh2 = stringDocumenth2.getBytes();
-
-                    MqttMessage mqttMessageh2 = new MqttMessage(arrayByteh2);
-                    mqttMessageh2.setQos(0);
-                    mqttMessageh2.setRetained(false);
-                    mqttClient.publish(topicoh2, mqttMessageh2);
-                    System.out.println(mqttMessageh2.toString());
-
-            } else {
-                return;
-            }
-
-
-
-        // Tratamento da coleção l1 local
-        localcollectionl1 = localMongoDatabase.getCollection(collectionl1);
-
-        Document dl1 = localcollectionl1.find().sort(new BasicDBObject("_id", -1)).first();
-
-            if (collectionl1.equals("l1")) {
-                Thread.sleep(5000);
-
-                    String zonal1 =  dl1.getString("Zona");
-                    String sensorl1 =  dl1.getString("Sensor");
-                    String datal1 =  dl1.getString("Data").replace("T", " ").replace("Z", " ");
-
-                    String medicaol1 = dl1.getString("Medicao");
-                    stringDocumentl1 = zonal1 + "|" + sensorl1 + "|" + datal1 + "|" + medicaol1;
-
-                    byte[] arrayBytel1 = stringDocumentl1.getBytes();
-
-                    MqttMessage mqttMessagel1 = new MqttMessage(arrayBytel1);
-                    mqttMessagel1.setQos(0);
-                    mqttMessagel1.setRetained(false);
-                    mqttClient.publish(topicol1, mqttMessagel1);
-                    System.out.println(mqttMessagel1.toString());
+                String zonah2 = dh2.getString("Zona");
+                String sensorh2 = dh2.getString("Sensor");
+                datah2 = dh2.getString("Data").replace("T", " ").replace("Z", " ");
+                String medicaoh2 = dh2.getString("Medicao");
+                String stringDocumenth2 = zonah2 + "|" + sensorh2 + "|" + datah2 + "|" + medicaoh2;
+                byte[] arrayByteh2 = stringDocumenth2.getBytes();
+                MqttMessage mqttMessageh2 = new MqttMessage(arrayByteh2);
+                mqttMessageh2.setQos(0);
+                mqttMessageh2.setRetained(false);
+                mqttClient.publish(topicoh2, mqttMessageh2);
+                System.out.println(mqttMessageh2.toString());
              } else {
                 return;
             }
-
-
-
+        }
+        // Tratamento da coleção l1 local
+        localcollectionl1 = localMongoDatabase.getCollection(collectionl1);
+        FindIterable<Document> newLocalMeasurementsl1;
+        if (lastDate == null) {
+            newLocalMeasurementsl1 = localcollectionl1.find();
+        } else {
+            newLocalMeasurementsl1 = localcollectionl1.find(Filters.gt("Data", lastDate));
+        }
+        for (Document dl1 : newLocalMeasurementsl1) {
+            if (collectionl1.equals("l1")) {
+                String zonal1 = dl1.getString("Zona");
+                String sensorl1 = dl1.getString("Sensor");
+                datal1 = dl1.getString("Data").replace("T", " ").replace("Z", " ");
+                String medicaol1 = dl1.getString("Medicao");
+                String stringDocumentl1 = zonal1 + "|" + sensorl1 + "|" + datal1 + "|" + medicaol1;
+                byte[] arrayBytel1 = stringDocumentl1.getBytes();
+                MqttMessage mqttMessagel1 = new MqttMessage(arrayBytel1);
+                mqttMessagel1.setQos(0);
+                mqttMessagel1.setRetained(false);
+                mqttClient.publish(topicol1, mqttMessagel1);
+                System.out.println(mqttMessagel1.toString());
+            } else {
+                return;
+            }
+        }
         // Tratamento da coleção l1 local
         localcollectionl2 = localMongoDatabase.getCollection(collectionl2);
-        Document dl2= localcollectionl2.find().sort(new BasicDBObject("_id", -1)).first();
-
+        FindIterable<Document> newLocalMeasurementsl2;
+        if (lastDate == null) {
+            newLocalMeasurementsl2 = localcollectionl2.find();
+        } else {
+            newLocalMeasurementsl2 = localcollectionl2.find(Filters.gt("Data", lastDate));
+        }
+        for (Document dl2 : newLocalMeasurementsl2) {
             if (collectionl2.equals("l2")) {
-                Thread.sleep(5000);
-
-                    String zonal2 = dl2.getString("Zona");
-                    String sensorl2 = dl2.getString("Sensor");
-                    String datal2 = dl2.getString("Data").replace("T", " ").replace("Z", " ");
-
-                    String medicaol2 = dl2.getString("Medicao");
-                    stringDocumentl2 = zonal2 + "|" + sensorl2 + "|" + datal2 + "|" + medicaol2;
-                    byte[] arrayBytel2 = stringDocumentl2.getBytes();
-
-                    MqttMessage mqttMessagel2 = new MqttMessage(arrayBytel2);
-                    mqttMessagel2.setQos(0);
-                    mqttMessagel2.setRetained(false);
-                    mqttClient.publish(topicol2, mqttMessagel2);
-                    System.out.println(mqttMessagel2.toString());
+                String zonal2 = dl2.getString("Zona");
+                String sensorl2 = dl2.getString("Sensor");
+                datal2 = dl2.getString("Data").replace("T", " ").replace("Z", " ");
+                String medicaol2 = dl2.getString("Medicao");
+                String stringDocumentl2 = zonal2 + "|" + sensorl2 + "|" + datal2 + "|" + medicaol2;
+                byte[] arrayBytel2 = stringDocumentl2.getBytes();
+                MqttMessage mqttMessagel2 = new MqttMessage(arrayBytel2);
+                mqttMessagel2.setQos(0);
+                mqttMessagel2.setRetained(false);
+                mqttClient.publish(topicol2, mqttMessagel2);
+                System.out.println(mqttMessagel2.toString());
 
             } else {
                 return;
             }
 
-
+        }
 
     }
     public static void main(String args[]) throws InterruptedException, MqttPersistenceException, MqttException {
@@ -285,10 +240,12 @@ public class MongoToMQTTTest {
         //Conexão ao MQTT
         while (true) {
             // obter doc mais recente de cada sensor comparar com a data em memÃ³ria
-            mtm.migrate();
-
-
-
+            mtm.migrate(datat1);
+            mtm.migrate(datat2);
+            mtm.migrate(datah1);
+            mtm.migrate(datah2);
+            mtm.migrate(datal1);
+            mtm.migrate(datah2);
             // converter mediÃ§Ã£o de cada sensor em string
 
             // enviar para o topico
---
+++ src/main/java/MongoToMongoTest.java
@@ -0,0 +1,83 @@
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Sorts;
+
+public class MongoToMongoTest {
+
+    // Aqui está o url para aceder aos dados da nuvem
+    String urlCloud = "mongodb://aluno:aluno@194.210.86.10:27017/?authSource=admin";
+    // Aqui está o url para colocar os dados nas réplicas do meu Mongo local
+    String urlLocal = "mongodb://localhost:27019,localhost:25019,localhost:23019/?replSet=replicaimdb";
+    // Aqui está o url para adicionar os dados numa só réplica do Mongo Local
+    String urlLocalUnique = "mongodb://localhost:27019";
+    // Base de dados da Nuvem
+    String cloudDatabase = "sid2021";
+    // Base de dados local
+    String localDatabase = "medicoes";
+
+    MongoClient cloudMongoClient = new MongoClient(new MongoClientURI(urlCloud));
+    MongoDatabase cloudMongoDatabase = cloudMongoClient.getDatabase(cloudDatabase);
+
+    MongoClient localClient = new MongoClient(new MongoClientURI(urlLocal));
+    MongoDatabase localMongoDatabase = localClient.getDatabase(localDatabase);
+
+    public void migrate(String sensorId) {
+
+        MongoCollection<Document> cloudCollection = cloudMongoDatabase.getCollection("sensor" + sensorId);
+        MongoCollection<Document> localCollection = localMongoDatabase.getCollection(sensorId);
+
+        FindIterable<Document> localList = localCollection.find().sort(Sorts.descending("_id")).limit(1);
+        Document last = localList.first();
+        System.out.println(last);
+
+        if (last != null) {
+            String lastDate = last.getString("Data");
+            long count = cloudCollection.countDocuments(Filters.gt("Data", lastDate));
+            System.out.println(count);
+            if (count > 0) {
+                FindIterable<Document> newMeasurements = cloudCollection.find(Filters.gt("Data", lastDate));
+                List<Document> list = new LinkedList<Document>();
+                for (Document d : newMeasurements) {
+                    list.add(d);
+                }
+                localCollection.insertMany(list);
+            }
+        } else {
+            System.out.println(cloudCollection.countDocuments());
+            FindIterable<Document> newMeasurements = cloudCollection.find();
+            List<Document> list = new LinkedList<Document>();
+            for (Document d : newMeasurements) {
+                list.add(d);
+            }
+            localCollection.insertMany(list);
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        MongoToMongoTest con = new MongoToMongoTest();
+        while (true) {
+            Thread.sleep(5000);
+            con.migrate("t1");
+            con.migrate("t2");
+            con.migrate("h1");
+            con.migrate("h2");
+            con.migrate("l1");
+            con.migrate("l2");
+        }
+    }
+}
